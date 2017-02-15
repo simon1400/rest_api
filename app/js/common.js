c8980685f86d8f4d16a74ff9daf353f3208b93d6
simon <pechunka11@gmail.com>
2017-02-15 13:38:53 +0100

    первый коммит

---
+++ app/js/common.js
@@ -0,0 +1,62 @@
+$(function() {
+
+	$('.mnu_list').hover(function() {
+		if($(this).attr('id') == 'mnu_0') hover_mnu('#hover_mnu_0', '#mnu_0');
+		else if($(this).attr('id') == 'mnu_1') hover_mnu('#hover_mnu_1', '#mnu_1');
+		else if($(this).attr('id') == 'mnu_2') hover_mnu('#hover_mnu_2', '#mnu_2');
+	},function() {
+		$('.hover_mnu').hover(function() {
+			$(this).css('display', 'block');},
+			function(){
+				$(this).css('display', 'none');
+			})
+		if($(this).attr('id') == 'mnu_0') $('#hover_mnu_0').css('display', 'none');
+		else if($(this).attr('id') == 'mnu_1') $('#hover_mnu_1').css('display', 'none');
+		else if($(this).attr('id') == 'mnu_2') $('#hover_mnu_2').css('display', 'none');
+	})
+
+	var ml;
+	function hover_mnu(param1, param2){
+		$(param1).css('display', 'block');
+		var param3 = param1 + " ul";
+		ml = $(param2).offset().left - 
+		($(param3).width() / 2) + 
+		($(param2).width() / 2) + 20;
+		$(param1).css('margin-left', ml + 'px');
+	}
+	
+
+
+	$('.wrap_hover_mnu').css('width', document.documentElement.clientWidth);
+
+	$('.nav_ask a span').hover(function() {
+		$('.targ_1').css('display', 'block');}, function(){
+			$('.targ_1').css('display', 'none');
+		})
+
+	//SVG Fallback
+	if(!Modernizr.svg) {
+		$("img[src*='svg']").attr("src", function() {
+			return $(this).attr("src").replace(".svg", ".png");
+		});
+	};
+
+	//E-mail Ajax Send
+	//Documentation & Example: https://github.com/agragregra/uniMail
+	$("form").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			alert("Thank you!");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
+
+});
